07fa3b6a0af97f8dd461d2e57b4812c798696016
cyrus01337 <cyrus21337@gmail.com>
2022-02-12 03:36:02 +0000

    Concisely wrap up Webpack rules

--- react/webpack.common.cjs
+++ react/webpack.common.cjs
@@ -4,33 +4,23 @@ const { merge } = require("webpack-merge");
 const common = require("../webpack.common.cjs");
 const utils = require("../utils.cjs");
 
-let rules = common.module.rules;
-
-for (let i = 0; i < rules.length; i++) {
-    // here, we numerically index in order to retrieve the entry from the rules array
-    // and perform direct mutations to it by proxy through the indexed entry
-    let rule = rules[i];
-    let pattern = rule.test;
-
-    if (!pattern.test(".mjs") || rule.use) continue;
-    // if (pattern.test(".mjs") && !rule.use) {
-
-    rule.test = /\.(mjs|jsx)/i;
-    rule.use = {
-        loader: "babel-loader",
-        options: {
-            presets: ["@babel/preset-env"]
-        }
-    };
-
-    break;
-}
-
 
 module.exports = merge(common, {
     module: {
         rules: [
-            ...rules
+            {
+                exclude: /node_modules/,
+                resolve: {
+                    fullySpecified: false
+                },
+                test: /\.(mjs|jsx)/i,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
         ]
     },
     resolve: {
